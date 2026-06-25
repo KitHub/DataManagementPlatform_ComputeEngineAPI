c1b5d040766b413468d6fa3624bfede810641bae
caoxudong <caoxudong818@gmail.com>
2026-06-25 20:51:27 +0800

    add cron task to remove useless bitmap local file

--- logic/compute_logic.go
+++ logic/compute_logic.go
@@ -3,9 +3,11 @@ package logic
 import (
 	"bufio"
 	"context"
+	"errors"
 	"log/slog"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/KitHub/DataManagementPlatform_ComputeEngineAPI/component"
@@ -14,16 +16,48 @@ import (
 	"github.com/RoaringBitmap/roaring/roaring64"
 )
 
+const bitmapLocalFileNameInfix = "bitmap"
+const bitmapLocalFileNameSeparator = "."
+const removeUselessBitmapLocalFileCronTaskName = "RemoveUselessBitmapLocalFileCronTask"
+
 var computeLogicInstance *ComputeLogic
 var onceForComputeLogicInstance sync.Once = sync.Once{}
 
-type ComputeLogic struct {
-	computeConfig *config.ComputeConfigEntity
-	packageLogic  *PackageLogic
-	packageMap    map[string]*roaring64.Bitmap
+type packageWithBitmapTuple struct {
+	packageEntity       *entity.PackageEntity
+	bitmap              *roaring64.Bitmap
+	bitmapLocalFilePath string
 }
 
-func NewComputeLogic(ctx context.Context, computeConfig *config.ComputeConfigEntity, packageLogic *PackageLogic, ossClient component.OSSComponent) (*ComputeLogic, error) {
+type ComputeLogic struct {
+	computeConfig *config.ComputeConfigEntity
+	cronComponent *component.CronComponent
+	packageLogic  *PackageLogic
+	packagesMap   *component.SyncMap[string, *packageWithBitmapTuple] // map[string]*packageWithBitmapTuple // key = packageEntity.OriginId
+}
+
+type removeUselessBitmapLocalFileCronTask struct {
+	packagesMap        *component.SyncMap[string, *packageWithBitmapTuple]
+	bitmapLocalFileDir string
+}
+
+func (t *removeUselessBitmapLocalFileCronTask) Run() {
+	ctx := context.Background()
+	slog.InfoContext(ctx, "begin task, "+removeUselessBitmapLocalFileCronTaskName)
+	err := removeUselessBitmapLocalFile(ctx, t.packagesMap, t.bitmapLocalFileDir)
+	if err != nil {
+		slog.ErrorContext(ctx, "task failed"+removeUselessBitmapLocalFileCronTaskName, slog.Any("error", err))
+	}
+	slog.InfoContext(ctx, "task done, "+removeUselessBitmapLocalFileCronTaskName)
+}
+func (t *removeUselessBitmapLocalFileCronTask) GetCronSpec() string {
+	return "0 */2 * * *"
+}
+func (t *removeUselessBitmapLocalFileCronTask) GetName() string {
+	return removeUselessBitmapLocalFileCronTaskName
+}
+
+func NewComputeLogic(ctx context.Context, computeConfig *config.ComputeConfigEntity, cronComponent *component.CronComponent, packageLogic *PackageLogic, ossClient component.OSSComponent) (*ComputeLogic, error) {
 	var err error = nil
 	onceForComputeLogicInstance.Do(func() {
 		err = os.MkdirAll(computeConfig.LocalBitmapDir, 0755)
@@ -31,16 +65,54 @@ func NewComputeLogic(ctx context.Context, computeConfig *config.ComputeConfigEnt
 			slog.ErrorContext(ctx, "create local bitmap dir failed", slog.String("localBitMapDir", computeConfig.LocalBitmapDir), slog.Any("error", err))
 		}
 
+		packagesMap := &component.SyncMap[string, *packageWithBitmapTuple]{} // make(map[string]*packageWithBitmapTuple)
+
 		computeLogicInstance = &ComputeLogic{
+			cronComponent: cronComponent,
 			computeConfig: computeConfig,
 			packageLogic:  packageLogic,
-			packageMap:    make(map[string]*roaring64.Bitmap),
+			packagesMap:   packagesMap,
+		}
+
+		cronTask := &removeUselessBitmapLocalFileCronTask{
+			packagesMap:        packagesMap,
+			bitmapLocalFileDir: computeConfig.LocalBitmapDir,
+		}
+
+		err = computeLogicInstance.cronComponent.Register(ctx, cronTask)
+		if err != nil {
+			slog.ErrorContext(ctx, "register cron task failed", slog.String("taskName", cronTask.GetName()), slog.Any("error", err))
 		}
 	})
 	return computeLogicInstance, err
 }
 
-func (logic *ComputeLogic) LoadPackages(ctx context.Context) error {
+func (logic *ComputeLogic) ReloadPackage(ctx context.Context, packageEntity *entity.PackageEntity, forceReloadFromOSS bool) error {
+	slog.InfoContext(ctx, "start reloading package", slog.Any("packageEntity", packageEntity))
+
+	bitmap, err := loadPackage(ctx, packageEntity, logic.computeConfig.LocalBitmapDir, logic.packageLogic.ossClient, forceReloadFromOSS)
+	if err != nil {
+		slog.ErrorContext(ctx, "force reloading package failed", slog.String("packageOriginId", packageEntity.OriginId), slog.Any("error", err))
+		return err
+	}
+
+	bitmapLocalFilePath, _, err := serializeBitMapToLocalFile(ctx, logic.computeConfig.LocalBitmapDir, packageEntity, bitmap)
+	if err != nil {
+		slog.ErrorContext(ctx, "force reloading package failed, serializing failed", slog.String("packageOriginId", packageEntity.OriginId), slog.Any("error", err))
+		return err
+	}
+
+	logic.packagesMap.Store(packageEntity.OriginId, &packageWithBitmapTuple{
+		packageEntity:       packageEntity,
+		bitmap:              bitmap,
+		bitmapLocalFilePath: bitmapLocalFilePath,
+	})
+
+	slog.InfoContext(ctx, "start reloading package done", slog.String("packageOriginId", packageEntity.OriginId))
+	return nil
+}
+
+func (logic *ComputeLogic) LoadAllPackages(ctx context.Context, forceReload bool) error {
 	var tmpId int64 = -1
 	var limit int32 = 10
 	var packages []*entity.PackageEntity = nil
@@ -60,19 +132,67 @@ func (logic *ComputeLogic) LoadPackages(ctx context.Context) error {
 	}
 
 	for _, tmpPackage := range packages {
-		bitmap, err := logic.loadPackageToBitmap(ctx, tmpPackage, logic.packageLogic.ossClient)
+		err := logic.ReloadPackage(ctx, tmpPackage, forceReload)
 		if err != nil {
 			return err
 		}
-
-		logic.packageMap[tmpPackage.OriginId] = bitmap
 	}
+
 	slog.InfoContext(ctx, "load packages to bitmap done")
 	return nil
 }
 
-func (logic *ComputeLogic) loadPackageToBitmap(ctx context.Context, packageEntity *entity.PackageEntity, ossClient component.OSSComponent) (*roaring64.Bitmap, error) {
-	tmpPackageFileName := "ProjectGenerator-" + packageEntity.OriginId + "-*"
+func idMappingContentToId(ctx context.Context, content string) (int64, error) {
+	// todo, add implmentation
+	panic("not implmented")
+}
+
+func serializeBitMapToLocalFile(ctx context.Context, bitmapLocalFileDir string, packageEntity *entity.PackageEntity, bitmap *roaring64.Bitmap) (localFilePath string, bytesCount int64, err error) {
+	bitmapLocalFile := composeBitmapLocalFilePath(ctx, bitmapLocalFileDir, packageEntity)
+	file, err := os.OpenFile(bitmapLocalFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		slog.ErrorContext(ctx, "open/create local bitmap file failed ", slog.Any("packageEntity", packageEntity), slog.String("bitmapLocalFile", bitmapLocalFile), slog.Any("error", err))
+		return "", 0, err
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			slog.ErrorContext(ctx, "close bitmap local file failed", slog.Any("packageEntity", packageEntity), slog.String("bitmapLocalFile", bitmapLocalFile), slog.Any("error", err))
+		}
+	}()
+
+	count, err := bitmap.WriteTo(file)
+	if err != nil {
+		slog.ErrorContext(ctx, "write local bitmap file failed ", slog.Any("packageEntity", packageEntity), slog.String("bitmapLocalFile", bitmapLocalFile), slog.Any("error", err))
+		return "", 0, err
+	}
+
+	slog.InfoContext(ctx, "serialize bitmap done", slog.Any("packageEntity", packageEntity), slog.String("bitmapLocalFile", bitmapLocalFile), slog.Any("bytesCount", count))
+	return bitmapLocalFile, count, nil
+}
+
+func composeBitmapLocalFilePath(ctx context.Context, bitmapLocalDir string, packageEntity *entity.PackageEntity) string {
+	return bitmapLocalDir + string(os.PathSeparator) + composeBitmapLocalFileName(ctx, packageEntity)
+}
+
+func composeBitmapLocalFileName(ctx context.Context, packageEntity *entity.PackageEntity) string {
+	return packageEntity.OriginId + bitmapLocalFileNameSeparator + bitmapLocalFileNameInfix + bitmapLocalFileNameSeparator + strconv.FormatInt(packageEntity.DataVersion, 10)
+}
+
+func decomposeBitmapLocalFileName(ctx context.Context, name string) (packageOriginId string, packageDataVersion string, err error) {
+	parts := strings.Split(name, bitmapLocalFileNameSeparator)
+	if len(parts) != 3 || parts[1] != bitmapLocalFileNameInfix {
+		errMsg := "decompose bitmap file name failed, invalid name format"
+		err = errors.New(errMsg)
+		slog.ErrorContext(ctx, errMsg, slog.String("bitmapLocalFileName", name))
+		return "", "", err
+	}
+
+	return parts[0], parts[2], nil
+}
+
+func loadBitmapFromOSS(ctx context.Context, packageEntity *entity.PackageEntity, ossClient component.OSSComponent) (*roaring64.Bitmap, error) {
+	tmpPackageFileName := "bitmap-" + packageEntity.OriginId + "-*"
 	tmpPackageFilePath, err := os.MkdirTemp("", tmpPackageFileName)
 	if err != nil {
 		slog.ErrorContext(ctx, "load package failed, create tmp package file faile", slog.String("tmpFileName", tmpPackageFileName), slog.Any("error", err))
@@ -91,7 +211,17 @@ func (logic *ComputeLogic) loadPackageToBitmap(ctx context.Context, packageEntit
 			slog.ErrorContext(ctx, "reading package file failed", slog.String("packageFile", tmpPackageFilePath), slog.Any("error", err))
 			return nil, err
 		}
-		defer tmpPackageFile.Close()
+		defer func() {
+			err := tmpPackageFile.Close()
+			if err != nil {
+				slog.ErrorContext(ctx, "close oss file failed", slog.String("ossFilePath", tmpPackageFilePath), slog.Any("error", err))
+			}
+			err = os.Remove(tmpPackageFilePath)
+			if err != nil {
+				slog.ErrorContext(ctx, "remove oss file failed", slog.String("ossFilePath", tmpPackageFilePath), slog.Any("error", err))
+			}
+			slog.InfoContext(ctx, "remove oss file done", slog.String("ossFilePath", tmpPackageFilePath))
+		}()
 
 		scanner := bufio.NewScanner(tmpPackageFile)
 		lineNo := 0
@@ -114,37 +244,92 @@ func (logic *ComputeLogic) loadPackageToBitmap(ctx context.Context, packageEntit
 		slog.InfoContext(ctx, "read content lines done", slog.String("pacakgeFile", tmpPackageFilePath), slog.Int64("lineCount", int64(lineNo)))
 	}
 
-	{
-		localBitmapFile := logic.computeConfig.LocalBitmapDir + "/" + packageEntity.OriginId + ".bitmap." + strconv.FormatInt(packageEntity.DataVersion, 10)
-		_, err = serializeBitMapToLocalFile(ctx, bitmap, localBitmapFile)
-		if err != nil {
-			slog.ErrorContext(ctx, "serialize bitmap to local file failed", slog.String("packageFile", tmpPackageFilePath), slog.Any("error", err))
-			return nil, err
-		}
-		slog.InfoContext(ctx, "serialize bitmap done", slog.Any("package", packageEntity), slog.String("packageBitmapFile", tmpPackageFilePath))
-	}
-
 	slog.InfoContext(ctx, "load package done", slog.String("pacakgeOriginId", packageEntity.OriginId))
 	return bitmap, nil
 }
 
-func idMappingContentToId(ctx context.Context, content string) (int64, error) {
-	// todo, add implmentation
-	panic("not implmented")
+func loadBitmapFromLocalFile(ctx context.Context, bitmapLocalFilePath string) (*roaring64.Bitmap, error) {
+	file, err := os.OpenFile(bitmapLocalFilePath, os.O_RDONLY, 0644)
+	if err != nil {
+		slog.ErrorContext(ctx, "open local bitmap file failed ", slog.String("localBitmapFile", bitmapLocalFilePath), slog.Any("error", err))
+		return nil, err
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			slog.ErrorContext(ctx, "close bitmap local file failed", slog.String("localBitmapFile", bitmapLocalFilePath), slog.Any("error", err))
+		}
+	}()
+	bitmap := roaring64.New()
+	bytesCount, err := bitmap.ReadFrom(file)
+	if err != nil {
+		slog.ErrorContext(ctx, "deserialize bitmap from local file failed", slog.String("localBitmapFile", bitmapLocalFilePath), slog.Any("error", err))
+		return nil, err
+	}
+	slog.InfoContext(ctx, "deserialize bitmap from local file done", slog.String("localBitmapFile", bitmapLocalFilePath), slog.Any("bytesCount", bytesCount))
+	return bitmap, nil
 }
 
-func serializeBitMapToLocalFile(ctx context.Context, bitmap *roaring64.Bitmap, filePath string) (bytesCount int64, err error) {
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil {
-		slog.ErrorContext(ctx, "open/create local bitmap file failed ", slog.String("localBitmapFile", filePath), slog.Any("error", err))
-		return 0, err
-	}
-	count, err := bitmap.WriteTo(file)
-	if err != nil {
-		slog.ErrorContext(ctx, "write local bitmap file failed ", slog.String("localBitmapFile", filePath), slog.Any("error", err))
-		return 0, err
+// loadPackage, load pacakge, if the bitmap local file existed and not expired, load bitmap from local file
+// forceReloadFromOSS, if it is true, reload from oss, skipping local file
+func loadPackage(ctx context.Context, targetPacakgeEntity *entity.PackageEntity, bitmapLocalFileDir string, ossClient component.OSSComponent, forceReloadFromOSS bool) (*roaring64.Bitmap, error) {
+	bitmapLocalFilePath := composeBitmapLocalFilePath(ctx, bitmapLocalFileDir, targetPacakgeEntity)
+
+	var retval *roaring64.Bitmap
+	if !forceReloadFromOSS {
+		retval, err := loadBitmapFromLocalFile(ctx, bitmapLocalFilePath)
+		if err == nil {
+			return retval, err
+		}
+
+		// if err not null, load bitmap from oss
 	}
 
-	slog.InfoContext(ctx, "serialize bitmap done", slog.String("localBitmapFile", filePath), slog.Any("bytesCount", count))
-	return count, nil
+	// load bitmap from oss, and write content to local file
+	retval, err := loadBitmapFromOSS(ctx, targetPacakgeEntity, ossClient)
+	return retval, err
+}
+
+// removeUselessBitmapLocalFile, with the process running and package changed, old-versioned bitmap file stayed in `bitmapLocalFileDir`
+// iterate SyncMap, mark useful file, and remove useless ones
+func removeUselessBitmapLocalFile(ctx context.Context, packagesMap *component.SyncMap[string, *packageWithBitmapTuple], bitmapLocalFileDir string) error {
+	entries, err := os.ReadDir(bitmapLocalFileDir)
+	if err != nil {
+		slog.ErrorContext(ctx, "read dir failed", slog.String("bitmapLocalFileDir", bitmapLocalFileDir), slog.Any("error", err))
+		return err
+	}
+
+	filesToBeRemoved := make([]string, 0)
+	for _, entry := range entries {
+		name := entry.Name()
+		if entry.IsDir() {
+			slog.DebugContext(ctx, "removeUselessBitmapLocalFile, skip dir", slog.String("bitmapLocalFileDir", bitmapLocalFileDir), slog.Any("dirName", name))
+		}
+
+		packageOriginId, packageDataVersoin, err := decomposeBitmapLocalFileName(ctx, name)
+		if err != nil {
+			slog.DebugContext(ctx, "removeUselessBitmapLocalFile, skip non-bitmap file", slog.String("bitmapLocalFileDir", bitmapLocalFileDir), slog.Any("dirName", name))
+			return err
+		}
+
+		tuple, ok := packagesMap.Load(packageOriginId)
+		if !ok {
+			filesToBeRemoved = append(filesToBeRemoved, bitmapLocalFileDir+string(os.PathSeparator)+name)
+			continue
+		}
+
+		if packageDataVersoin != strconv.FormatInt(tuple.packageEntity.DataVersion, 10) {
+			filesToBeRemoved = append(filesToBeRemoved, bitmapLocalFileDir+string(os.PathSeparator)+name)
+			continue
+		}
+	}
+
+	for _, filePath := range filesToBeRemoved {
+		err := os.Remove(filePath)
+		if err != nil {
+			slog.ErrorContext(ctx, "remove useless bitmap local file failed", slog.Any("error", err))
+		}
+	}
+
+	return nil
 }
